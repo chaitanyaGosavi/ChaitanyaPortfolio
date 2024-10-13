225c593cb8fb4551b15f19f439f890373264f08e
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2024-10-13 20:59:06 +0530

    trial of paths 1

--- src/data/dataObj.js
+++ src/data/dataObj.js
@@ -34,7 +34,7 @@ var response = {
     ],
     skillsResponse: [
         {
-            imgPath: "src/assets/reactBG.png",
+            imgPath: "/src/assets/reactBG.png",
             id: 1
         }, {
             imgPath: "src/assets/javascriptBG.png",
